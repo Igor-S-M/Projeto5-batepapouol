73e74df9302fc97d0dd6ddc93e4b4ab9058a322f
Igor Sousa <igorsousa3148@gmail.com>
2022-08-26 08:39:18 -0300

    layout feito

---
+++ script.js
@@ -0,0 +1,11 @@
+let mensagem = document.querySelector(".feed")
+
+mensagem.innerHTML = `
+<div class="mensagem">
+    <div class="data">(09:21:45)</div>
+    <div class="endereco"> <h1>João</h1> para <h1>Todos</h1>: </div>
+    <div class="texto"> Bom diaaaaaaaaaa!</div>
+</div>
+`
+
+
